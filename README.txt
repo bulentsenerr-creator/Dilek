9582635c899a5f75b7547932fd262e6a2570fff3
bulentsenerr-creator <bulentsenerr@gmail.com>
2026-01-31 13:02:03 +0300

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,7 @@
+MasterKoleksiyon - PWA Paket
+
+ÖNEMLİ: Service Worker yalnızca HTTPS veya geliştirmede localhost üzerinde çalışır. file:// ile açmayın.
+
+Android Chrome test:
+- Siteyi normal sekmede açın
+- Eğer daha önce PWA kurduysanız ve butonlar çalışmıyorsa: Chrome > Ayarlar > Site ayarları > Depolama/Site verisi temizleyin, sonra yeniden deneyin.
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/* MasterKoleksiyon PWA Service Worker */
+const CACHE_NAME = 'masterkoleksiyon-v34.6.13';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './service-worker.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    Promise.all([
+      caches.keys().then(keys => Promise.all(keys.map(k => (k !== CACHE_NAME) ? caches.delete(k) : null))),
+      self.clients.claim()
+    ])
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  event.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
